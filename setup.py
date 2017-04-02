142e61d718e89704ff199a7e0301e2719141faed
JulieRossi <julierossi06@gmail.com>
2017-04-02 17:37:46 +0200

    use git tag

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
+
 from setuptools import setup
 
-setup(name='agnostic_loader',
-      version='0.3.1',
-      description='Load data from different input sources',
-      url='https://github.com/JulieRossi/agnostic_loader.git',
-      author='Julie Rossi',
-      author_email='julierossi06@gmail.com',
-      license='MIT',
-      packages=['agnostic_loader'],
-      zip_safe=False)
+version = os.getenv("TRAVIS_TAG")
+
+if version:
+    setup(name='agnostic_loader',
+          version=version,
+          description='Load data from different input sources',
+          url='https://github.com/JulieRossi/agnostic_loader.git',
+          author='Julie Rossi',
+          author_email='julierossi06@gmail.com',
+          license='MIT',
+          packages=['agnostic_loader'],
+          zip_safe=False)
